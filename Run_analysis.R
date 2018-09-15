dd95e1a90455bc9ff94bd324b174261830222618
GEETHUV <41547849+GEETHUV@users.noreply.github.com>
2018-09-15 17:27:56 +0530

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,1 +1,12 @@
+library(dplyr)
 
+##############################################################################################
+# STEP 1  Getting data
+
+# downloading zip file if it hasn't already been downloaded
+zUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zFile <- "UCI HAR Dataset.zip"
+
+if (!file.exists(zFile)) {
+  download.file(zUrl, zFile, mode = "wb")
+}
